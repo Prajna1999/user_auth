91ec989becd9fc4ea9ac9f334eaa876d31ca329b
Prajna1999 <gituprajna20@gmail.com>
2023-07-19 23:42:09 +0530

    hotfix

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,10 +4,10 @@ const usersDB = {
 }
 const bcrypt = require('bcrypt');
 
-const jwt=require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
-const fsPromises=require('fs').promises;
-const path=require('path')
+const fsPromises = require('fs').promises;
+const path = require('path');
 
 const handleLogin = async (req, res) => {
     const { user, pwd } = req.body;
@@ -18,38 +18,26 @@ const handleLogin = async (req, res) => {
     const match = await bcrypt.compare(pwd, foundUser.password);
     if (match) {
         // create JWTs
-        const accessToken=jwt.sign(
-            {
-                "username":foundUser.username,
-
-
-            },
+        const accessToken = jwt.sign(
+            { "username": foundUser.username },
             process.env.ACCESS_TOKEN_SECRET,
-            {expiresIn:'30s'}
+            { expiresIn: '30s' }
         );
-        const refreshToken=jwt.sign(
-            {
-                "username":foundUser.username,
-
-
-            },
+        const refreshToken = jwt.sign(
+            { "username": foundUser.username },
             process.env.REFRESH_TOKEN_SECRET,
-            {expiresIn:'1d'}
+            { expiresIn: '1d' }
         );
-        //saving refresh token with current user
-        const otherUsers=usersDB.users.filter(person=>person.username!==foundUser.username);
-        const currUser={...foundUser, refreshToken};
-        usersDB.setUsers([...otherUsers, currUser])
-        res.json({ 'success': `User ${user} is logged in!` });
+        // Saving refreshToken with current user
+        const otherUsers = usersDB.users.filter(person => person.username !== foundUser.username);
+        const currentUser = { ...foundUser, refreshToken };
+        usersDB.setUsers([...otherUsers, currentUser]);
         await fsPromises.writeFile(
             path.join(__dirname, '..', 'model', 'users.json'),
             JSON.stringify(usersDB.users)
         );
-        //below cookie not available to JS
-        res.cookie('jwt', refreshToken, {httpOnly:true, maxAge:24*60*60*1000})
-        // access token stored in memory not in localStorage.
-        // access token to the frontend
-        res.json({accessToken});
+        res.cookie('jwt', refreshToken, { httpOnly: true, sameSite: 'None', secure: true, maxAge: 24 * 60 * 60 * 1000 });
+        res.json({ accessToken });
     } else {
         res.sendStatus(401);
     }
